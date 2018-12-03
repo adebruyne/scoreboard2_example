948829e9e9c74d67463a5e5acbf97665e23e3987
Aylin DeBruyne <adebruyne88@gmail.com>
2018-12-03 10:44:28 -0500

    gives each player a card to display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,56 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
 
 class App extends Component {
-  //add a comment
+  constructor(props) {
+    super(props);
+
+    //Set up state
+    this.state = {
+      scores: [
+        {
+          id: 1,
+          name: "ahjuma",
+          score: 1001
+        },
+        {
+          id: 2,
+          name: "jeffles",
+          score: 20
+        },
+        {
+          id: 3,
+          name: "stinkface",
+          score: 500
+        }
+      ]
+    };
+  }
+
   render() {
-    return <div className="App" />;
+    return (
+      <div className="App">
+        {" "}
+        {/* here is another comment */}
+        <h1>Scoreboard App</h1>
+        {/* This is the comment: command + /   */}
+        <div className="score-container">{this._scoresAsCards()}</div>
+      </div>
+    );
+  }
+
+  _scoresAsCards() {
+    const cards = this.state.scores.map(score => {
+      return (
+        <div key={score.id}>
+          <h2>
+            Name: {score.name} // Score: {score.score}
+          </h2>
+        </div>
+      );
+    });
+    return cards;
   }
 }
 
